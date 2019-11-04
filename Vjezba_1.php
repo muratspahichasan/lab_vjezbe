ed7e7698297446c66b4939841ac86cf59df3ccb4
Hasan <hasanmuratspahic@gmail.com>
2019-11-04 22:13:28 +0100

    Update Vjezba_1.php

---
+++ Vjezba_1.php
@@ -0,0 +1,7 @@
+<?php
+    $ime_prezime = ("Hasan Muratspahić");
+    $mjesto_rodjenja = ("Livno");
+    $kanton = ("Kanton 10");
+    $drzava = ("Bosna i HErcegovina");
+    echo ("ime_prezime");
+?>
