cb43bda7d840138c94ffc7b9b5807827719f128c
Hasan <hasanmuratspahic@gmail.com>
2019-11-30 01:13:52 +0100

    Vjezba_leno

---
+++ vjezba_leno_stranica/web/index.html.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- SEO Meta Tags -->
+    <meta name="description" content="Free mobile app HTML landing page template to help you build a great online presence for your app which will convert visitors into users">
+    <meta name="author" content="Inovatik">
+
+    <!-- OG Meta Tags to improve the way the post looks when you share the page on LinkedIn, Facebook, Google+ -->
+	<meta property="og:site_name" content="" /> <!-- website name -->
+	<meta property="og:site" content="" /> <!-- website link -->
+	<meta property="og:title" content=""/> <!-- title shown in the actual shared post -->
+	<meta property="og:description" content="" /> <!-- description shown in the actual shared post -->
+	<meta property="og:image" content="" /> <!-- image link, make sure it's jpg -->
+	<meta property="og:url" content="" /> <!-- where do you want your post to link to -->
+	<meta property="og:type" content="article" />
+
+    <!-- Website Title -->
+    <title>Leno - Mobile App HTML Landing Page Template</title>
+    
+    <!--Start of Styles -->
+    <?php require 'styles.html.php'; ?>
+    <!--End of Styles -->
+</head>
+<body data-spy="scroll" data-target=".fixed-top">
+    
+    <!-- Start of preloader -->
+	<?php require 'preloader.html.php'; ?>
+    <!-- end of preloader -->
+    
+
+    <!--Start of navigation -->
+   <?php
+    require 'navigation.html.php';
+   ?>
+    <!-- end of navigation -->
+
+
+    <!-- Start of header -->
+   <?php require 'header.html.php' ;
+   ?>
+    <!-- end of header -->
+
+
+    <!-- Start of testimonials -->
+    <?php require 'slider.html.php'; ?>
+    <!-- end of testimonials -->
+    
+
+    <!--Start of features -->
+    <?php require 'features.html.php'; ?>
+    <!-- end of features -->
+
+
+    <!--Start of video -->
+    <?php require 'video.html.php'; ?>
+    <!-- end of video -->
+
+
+    <!-- Start of Details 1 -->
+    <?php require 'details_1.html.php'; ?>
+    <!-- end of details 1 -->
+
+
+    <!--Start of Details 2 -->
+   <?php require 'details_2.html.php'; ?>  
+    <!-- end of details 2 -->
+
+
+    <!-- Start of Details Lightboxes -->
+    <?php require 'details_lightbox.html.php'; ?>
+    <!-- end of lightbox -->
+    
+    <!--Start of Lightbox -->
+    <?php require 'lightbox.html.php'; ?>
+    <!-- end of details lightboxes -->
+
+
+    <!-- Start of screenshots -->
+    <?php require 'screenshots.html.php'; ?>
+    <!-- end of screenshots -->
+
+
+    <!--Start of Download -->
+   <?php require 'download.html.php'; ?>
+    <!-- end of download -->
+
+
+    <!--Start of Statistics -->
+    <?php require 'statistics.html.php'; ?>
+    <!-- end of statistics -->
+
+
+    <!--Start of Contact -->
+    <?php require 'contact.html.php'; ?>
+    <!-- end of contact -->
+
+
+    <!--Start of Footer -->
+    <?php require 'footer.html.php'; ?>
+    <!-- end of footer -->
+
+
+    <!-- Start of Copyright -->
+    <?php require 'copyright.html.php'; ?>
+    <!-- end of copyright -->
+    
+    	
+    <!--Start of  Scripts -->
+   <?php require 'scripts.html.php'; ?>
+   <!--End of Scripts-->
+</body>
+</html>
